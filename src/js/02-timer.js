209e4a01cc1956108301aa65b0751923a6a7ee61
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-06-11 13:27:23 +0200

    Task 03 - all tasks completed
    Yeah baby, i'm on a roll!

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,118 @@
-// Initializing flatpickr library and css ruleset 
-import flatpickr from "flatpickr";
+// Initializing flatpickr library and css ruleset - datetime picker
+import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-// Flatpickr options 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    minDate: "today",
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+// Initializing Notiflix library - Notify, Report and Block
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
 
-// Selecting input for datetime picker
-flatpickr("#datetime-picker", options);
+// Variables initiation
+const startBtn = document.querySelector('[data-start]');
+const daysOutput = document.querySelector('[data-days]');
+const hoursOutput = document.querySelector('[data-hours]');
+const minutesOutput = document.querySelector('[data-minutes]');
+const secondsOutput = document.querySelector('[data-seconds]');
+let datetimePicker = document.querySelector('#datetime-picker'); //did this to be able to disable input
+let selectedDateTime = 0;
+let timer = null;
+
+// Disabling start button as default
+startBtn.disabled = true;
+
+// Function for time data conversion to object
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// Flatpickr options
+const options = {
+  position: 'auto center',
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  // Preventing improper countdown behavior
+  onOpen() {
+    startBtn.disabled = true;
+  },
+  onClose(selectedDates) {
+    if (selectedDates[0] > Date.now()) {
+      startBtn.disabled = false;
+      selectedDateTime = selectedDates[0] - 0; //Subtracting 0 converts data to unix format
+      Notify.info('Date & time set, ready for countdown');
+    } else {
+      Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    }
+  },
+};
+
+const notiflixOptions = {};
+
+// Fix for displaying remaining time as two-digit number
+function addLeadingZero(value) {
+  const string = String(value);
+  return string.padStart(2, '0');
+}
+
+// Function for countdown display handling - Start button event handler,
+// Starting countdown and updating time at 1000ms interval
+function countdown(evt) {
+  // Preventing improper countdown behavior, callback duplication,
+  // and checking if date & time is still valid
+  startBtn.disabled = true;
+  if (selectedDateTime < Date.now()) {
+    Report.failure(
+      'Countdown start impossible!',
+      'Set date & time older than present date & time. You need to be faster!',
+      'Okie dokie...'
+    );
+
+    return;
+  }
+  datetimePicker.disabled = true;
+  Block.hourglass('[block]', 'Countdown in progress...');
+
+  timer = setInterval(() => {
+    let timeLeft = selectedDateTime - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+    daysOutput.innerHTML =
+      String(days).length < 2 ? addLeadingZero(days) : days;
+    hoursOutput.innerHTML =
+      String(hours).length < 2 ? addLeadingZero(hours) : hours;
+    minutesOutput.innerHTML =
+      String(minutes).length < 2 ? addLeadingZero(minutes) : minutes;
+    secondsOutput.innerHTML =
+      String(seconds).length < 2 ? addLeadingZero(seconds) : seconds;
+
+    if (timeLeft < 1000) {
+      clearInterval(timer);
+      Notify.success('Countdown exectuted successfully');
+      Block.remove('[block]');
+      datetimePicker.disabled = false;
+    }
+  }, 1000);
+}
+
+//Initializing datetime picker
+input = flatpickr('#datetime-picker', options);
+
+// Initializing listener for start button
+startBtn.addEventListener('click', countdown);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,64 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// Initializing Notiflix - Notify and Report modules
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+// Selecting input form
+const form = document.querySelector('.form');
+
+// Initializing timer for promises generation delay
+let timer = 0;
+
+// Letting user know, that all input values must be greater than 0
+Notify.info('Please input values greater than 0. Thank You!');
+
+// Function for generating promises
+function createPromise(i, timer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ i, timer });
+      } else {
+        reject({ i, timer });
+      }
+    }, timer);
+  });
 }
+
+// Function for handling form input data and using callback for generating promises
+const promisesHandler = event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget.elements;
+
+  const firstDelay = Number(delay.value);
+  const stepDelay = Number(step.value);
+  const amountOfPromises = Number(amount.value);
+
+  // Checking for valid input - values greater than 0
+  if (firstDelay <= 0 || stepDelay <= 0 || amountOfPromises <= 0) {
+    delay.value = null;
+    step.value = null;
+    amount.value = null;
+
+    return Report.failure(
+      'Oops! I did it again... Or not?',
+      'All values must be greater than zero!',
+      'If you say so...'
+    );
+  }
+
+  timer = firstDelay;
+  for (let i = 1; i <= amountOfPromises; i++) {
+    createPromise(i, timer)
+      .then(({ i, timer }) => {
+        Notify.success(`Fulfilled promise ${i} in ${timer}ms`);
+      })
+      .catch(({ i, timer }) => {
+        Notify.failure(`Rejected promise ${i} in ${timer}ms`);
+      });
+    timer += stepDelay;
+  }
+};
+
+form.addEventListener('submit', promisesHandler);
